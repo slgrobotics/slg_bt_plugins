cf4428cd9f09533a5fe12f9c6d3c32c39d0a4d66
Sergei Grichine <msghub@hotmail.com>
2025-12-21 15:20:43 -0600

    Update a_node_registration.cpp

--- src/a_node_registration.cpp
+++ src/a_node_registration.cpp
@@ -2,6 +2,7 @@
 #include <behaviortree_cpp/bt_factory.h>
 
 #include "slg_bt_plugins/fgs_to_bt_node.hpp"
+#include "slg_bt_plugins/dump_blackboard_data.hpp"
 #include "slg_bt_plugins/is_face_detected_condition.hpp"
 #include "slg_bt_plugins/is_stop_gesture_condition.hpp"
 #include "slg_bt_plugins/turn_toward_face_action.hpp"
@@ -11,6 +12,7 @@
 BT_REGISTER_NODES(factory)
 {
   factory.registerNodeType<slg_bt_plugins::FgsTopicToBlackboard>("FgsTopicToBlackboard");
+  factory.registerNodeType<slg_bt_plugins::DumpBlackboardData>("DumpBlackboardData");
   factory.registerNodeType<slg_bt_plugins::TurnTowardFace>("TurnTowardFace");
   factory.registerNodeType<slg_bt_plugins::IsStopGesture>("IsStopGesture");
   factory.registerNodeType<slg_bt_plugins::IsFaceDetected>("IsFaceDetected");
